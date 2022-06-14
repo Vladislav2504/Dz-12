1a30e5ae15456b201f4dd2fd0823476caae127f9
Frontmen1 <vital199617@gmail.com>
2022-06-14 18:59:24 +0300

    modern

--- src/Documents/Main.java
+++ src/Documents/Main.java
@@ -1,28 +1,42 @@
 package Documents;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
+        System.out.println("Введите название или номер документа.");
         Scanner scanner1 = new Scanner(System.in);
         String n = scanner1.next();
 
-        try(FileReader file = new FileReader(n)) {
+
+        try (FileReader file = new FileReader(n)) {
             Scanner scanner = new Scanner(file);
-
+            FileWriter writer1 = new FileWriter("Adopted.txt");
+            FileWriter writer2 = new FileWriter("Error.txt");
             while (scanner.hasNextLine()) {
-                System.out.println((scanner.nextLine().startsWith("docnum")) || (scanner.nextLine().startsWith("contract"))
-                        && (scanner.nextLine().length() == 15));
-                System.out.println();
-            }
-            System.out.println();
+                String a = scanner.nextLine();
+                if (((a.startsWith("docnum")) || (a.startsWith("contract")))
+                        && (a.length() == 15)) {
 
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+                    writer1.write(a+"\n");
+                    System.out.print(a);
+                    System.out.println(" | Adopted");
+
+
+                } else {
+
+                    writer2.write(a+"\n");
+                    System.out.print(a);
+                    System.out.println(" | Error");
+                }
+
+
+            }
+            writer1.close();
+            writer2.close();
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
